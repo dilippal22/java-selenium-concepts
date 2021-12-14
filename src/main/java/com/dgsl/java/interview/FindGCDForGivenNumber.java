959fd5255d0db36dc5279a8d93b75676dbe282d7
Dilip Pal <dilippal22@gmail.com>
2021-12-14 19:32:22 +0530

    find GCD number of any two number

---
+++ src/main/java/com/dgsl/java/interview/FindGCDForGivenNumber.java
@@ -0,0 +1,46 @@
+/*Write a Java program to find GCD(Greatest Common Divisor) for the given number.
+ * Input1 : 15; Input2 : 25
+ * */
+
+package com.dgsl.java.interview;
+
+import java.util.Scanner;
+
+public class FindGCDForGivenNumber {
+
+	// static int inputNum1;
+	// static int inputNum2;
+
+	public static int findGCD(int inputNum1, int inputNum2) {
+
+		if (inputNum1 == 0)
+			return inputNum2;
+		if (inputNum2 == 0)
+			return inputNum1;
+
+		if (inputNum1 == inputNum2)
+			return inputNum1;
+
+		if (inputNum1 > inputNum2)
+			return findGCD(inputNum1 - inputNum2, inputNum2);
+		return findGCD(inputNum1, inputNum2 - inputNum1);
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter First input number: ");
+		int inputNum1 = sc.nextInt();
+
+		System.out.println("Enter second input number: ");
+		int inputNum2 = sc.nextInt();
+
+		System.out.println(findGCD(inputNum1, inputNum2));
+
+		sc.close();
+
+	}
+
+}
